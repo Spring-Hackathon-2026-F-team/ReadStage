82a3f89a4ac9bdbb33c10db9aad45b71e85c47ac
nekoko3 <e.tanaka.ism@gmail.com>
2026-04-26 21:14:11 +0900

    ログイン・新規登録処理実装

--- ReadStage/models.py
+++ ReadStage/models.py
@@ -20,3 +20,45 @@ class Book:
       abort(500)
     finally:
       db_pool.release(conn)
+
+class User:
+  # メールアドレスに合致するユーザーIDとパスワードのみを返却
+  @classmethod
+  def find_user_by_email(cls, email):
+    conn = db_pool.get_conn()
+    try:
+      with conn.cursor() as cur:
+        sql = 'SELECT id, password FROM users WHERE email=%s;'
+        cur.execute(sql, (email))
+        user = cur.fetchone()
+      return user
+    except pymysql.Error as e:
+      print(f'エラーが発生しています：{e}')
+      abort(500)
+    finally:
+      db_pool.release(conn)
+    
+  # DB登録後ユーザーIDを返却
+  @classmethod
+  def create(cls, email, hashed_password):
+    conn = db_pool.get_conn()
+    try:
+      with conn.cursor() as cur:
+        sql = 'INSERT INTO users (email, password) VALUES (%s, %s);'
+        cur.execute(sql, (email, hashed_password))
+        conn.commit()
+
+      # insertしたユーザーIDを取得
+      # user_idがautoincrementなのでcur.lastrowidでも良いが、
+      # UUIDに変更した時のために再取得して返却する
+      user = cls.find_user_by_email(email)
+      if user is not None:
+        return user['id']
+
+      # insertしたデータのユーザーが取れない（＝イレギュラーケース）は500エラーに流す
+      raise
+    except pymysql.Error as e:
+      print(f'エラーが発生しています：{e}')
+      abort(500)
+    finally:
+      db_pool.release(conn)
