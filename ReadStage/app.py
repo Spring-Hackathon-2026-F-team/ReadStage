112865e401ad5efb1e975e8b9b89dd2d5bdf0f38
nekoko3 <e.tanaka.ism@gmail.com>
2026-05-03 20:50:19 +0900

    画面名コメント修正

--- ReadStage/app.py
+++ ReadStage/app.py
@@ -136,14 +136,14 @@ def books_view():
         return render_template('book/books.html', books=books)
 
 
-# 書籍評価ページ表示
+# 評価詳細ページ表示
 @app.route("/book/<int:book_id>", methods=["GET"])
 def book_id_view(book_id):
 
     return render_template('book/book_detail.html')
 
 
-# 書籍評価投稿ページ表示
+# 書籍コメントページ表示
 @app.route("/book/<int:book_id>/comment", methods=["GET"])
 def book_comment_view(book_id):
     # セッションチェック
